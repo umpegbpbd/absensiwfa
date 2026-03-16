dfe97ee5ab884e52cb44cf46afeb319b104386bf
umpegbpbd <radityasukmana81@gmail.com>
2026-03-16 20:25:18 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// =======================
+// LOAD DATA DARI GITHUB
+// =======================
+
 async function loadData(){
 
 let url = `https://api.github.com/repos/${CONFIG.OWNER}/${CONFIG.REPO}/contents/${CONFIG.DATA_FILE}`
@@ -9,5 +13,126 @@ let data = await res.json()
 let content = JSON.parse(atob(data.content))
 
 return content
+
+}
+
+
+
+// =======================
+// SIMPAN DATA KE GITHUB
+// =======================
+
+async function saveData(data){
+
+let url = `https://api.github.com/repos/${CONFIG.OWNER}/${CONFIG.REPO}/contents/${CONFIG.DATA_FILE}`
+
+let get = await fetch(url)
+
+let file = await get.json()
+
+let sha = file.sha
+
+let content = btoa(JSON.stringify(data,null,2))
+
+await fetch(url,{
+method:"PUT",
+headers:{
+Authorization:"token "+CONFIG.TOKEN
+},
+body:JSON.stringify({
+message:"update absensi",
+content:content,
+sha:sha
+})
+})
+
+}
+
+
+
+// =======================
+// AKTIFKAN KAMERA
+// =======================
+
+let video = document.getElementById("camera")
+
+navigator.mediaDevices.getUserMedia({video:true})
+.then(stream=>{
+video.srcObject = stream
+})
+
+
+
+// =======================
+// AMBIL FOTO
+// =======================
+
+function capture(){
+
+let canvas = document.getElementById("canvas")
+
+canvas.width = video.videoWidth
+canvas.height = video.videoHeight
+
+let ctx = canvas.getContext("2d")
+
+ctx.drawImage(video,0,0)
+
+return canvas.toDataURL("image/jpeg",0.3)
+
+}
+
+
+
+// =======================
+// AMBIL GPS
+// =======================
+
+function getLocation(){
+
+return new Promise((resolve,reject)=>{
+
+navigator.geolocation.getCurrentPosition(pos=>{
+
+resolve({
+lat:pos.coords.latitude,
+lon:pos.coords.longitude
+})
+
+})
+
+})
+
+}
+
+
+
+// =======================
+// ABSENSI
+// =======================
+
+async function absen(){
+
+let nama = document.getElementById("pegawai").value
+
+let foto = capture()
+
+let lokasi = await getLocation()
+
+let data = await loadData()
+
+data.push({
+
+nama:nama,
+tanggal:new Date().toISOString(),
+foto:foto,
+latitude:lokasi.lat,
+longitude:lokasi.lon
+
+})
+
+saveData(data)
+
+alert("Absensi berhasil")
 
 }
