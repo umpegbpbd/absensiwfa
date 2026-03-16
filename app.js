c41c9a63ab4803847ab084844723d284e4eefe20
umpegbpbd <radityasukmana81@gmail.com>
2026-03-16 20:22:48 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+async function loadData(){
 
+let url = `https://api.github.com/repos/${CONFIG.OWNER}/${CONFIG.REPO}/contents/${CONFIG.DATA_FILE}`
+
+let res = await fetch(url)
+
+let data = await res.json()
+
+let content = JSON.parse(atob(data.content))
+
+return content
+
+}
