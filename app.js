24bb617470d4e37ad4c5da6c45446038b7978817
umpegbpbd <radityasukmana81@gmail.com>
2026-03-17 11:57:33 +0700

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ let state = {
 };
 
 const DEFAULT_SUBMIT_LABEL = "✓ Kirim Data";
-const TOKEN_STORAGE_KEY = "ABSENSI_GITHUB_TOKEN";
+const TOKEN_STORAGE_KEY = "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";
 
 window.addEventListener("DOMContentLoaded", () => {
     startClock();
