c0cb8c6038ee0bd2ca4b7fa21132c56368af41a5
umpegbpbd <radityasukmana81@gmail.com>
2026-03-17 13:28:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,10 @@
 const CONFIG = {
-APP_NAME: "Absensi WFA",
-ORGANISASI: "BPBD KAB. TRENGGALEK",
-ADMIN_PASSWORD: "admin123",
+  ADMIN_PASSWORD: "admin123",
 
-OWNER: "umpegbpbd",
-REPO: "absensiwfa",
-BRANCH: "main",
-DATA_FILE: "data/absensi.json",
+  OWNER: "umpegbpbd",
+  REPO: "absensiwfa",
+  BRANCH: "main",
+  DATA_FILE: "data/absensi.json",
 
-// KOSONGKAN dulu biar tidak error
-TOKEN: "",
-
-JAM_MASUK_END: "07:30",
-JAM_PULANG_START: "15:30",
-
-IMAGE_QUALITY: 0.45,
-CAMERA_FACING_MODE: "user"
+  TOKEN: "" // kosongkan dulu
 };
