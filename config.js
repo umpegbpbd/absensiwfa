0aff3ce7f69e3227068d70844a8ffb262f6c7432
umpegbpbd <radityasukmana81@gmail.com>
2026-03-17 14:22:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,8 @@ const CONFIG = {
   OWNER: "umpegbpbd",
   REPO: "absensiwfa",
   BRANCH: "main",
-  DATA_FILE: "data/absensi.json",
+  DATA_FILE: "data/absensi.json", // File ini akan terbuat otomatis jika belum ada
 
-  TOKEN: "" // kosongkan dulu
+  // GANTI TOKEN DI BAWAH INI DENGAN TOKEN GITHUB KAMU
+  TOKEN: "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" 
 };
