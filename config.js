892dd82dc65fbc706a4df04f24dff4b9d966c52b
umpegbpbd <radityasukmana81@gmail.com>
2026-03-17 13:10:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,20 +3,17 @@ APP_NAME: "Absensi WFA",
 ORGANISASI: "BPBD KAB. TRENGGALEK",
 ADMIN_PASSWORD: "admin123",
 
-// GitHub storage
 OWNER: "umpegbpbd",
 REPO: "absensiwfa",
 BRANCH: "main",
 DATA_FILE: "data/absensi.json",
 
-// Token GitHub
-TOKEN: "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",
+// KOSONGKAN dulu biar tidak error
+TOKEN: "",
 
-// Pengaturan absensi
 JAM_MASUK_END: "07:30",
 JAM_PULANG_START: "15:30",
 
-// Kamera & kualitas gambar
 IMAGE_QUALITY: 0.45,
 CAMERA_FACING_MODE: "user"
 };
